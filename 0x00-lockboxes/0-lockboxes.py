3215dd8ea36e0f980b38b63c53f49bad68e10018
Alejo Ortiz <davidalejandro64@gmail.com>
2020-07-22 00:25:13 -0500

    add lockboxes

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+def canUnlockAll(boxes):
+    """function that determines if we can open all boxes"""
+    open_boxes = [0]
+    for key in open_boxes:
+        for box in boxes[key]:
+            if box not in open_boxes and box < len(boxes):
+                open_boxes.append(box)
+    if len(open_boxes) == len(boxes):
+        return True
+    return False
